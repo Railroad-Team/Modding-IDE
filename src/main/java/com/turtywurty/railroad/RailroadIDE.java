63edfdcd1b8f8b59f47de685f578bfae7f0d2619
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2021-02-07 20:09:49 +0000

    Primary Project Files

---
+++ src/main/java/com/turtywurty/railroad/RailroadIDE.java
@@ -0,0 +1,16 @@
+package com.turtywurty.railroad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RailroadIDE extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage mainWindow) throws Exception {
+		
+	}
+}
