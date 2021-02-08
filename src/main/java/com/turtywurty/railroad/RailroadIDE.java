a7178d937740400470b4f36a750f992204974727
Ninjaeevee500 <Ninjaeevee500@users.noreply.github.com>
2021-02-08 11:07:24 +0000

    Moved Menu from the main class to its own seperate class

--- src/main/java/com/turtywurty/railroad/RailroadIDE.java
+++ src/main/java/com/turtywurty/railroad/RailroadIDE.java
@@ -4,20 +4,18 @@ import java.io.InputStream;
 
 import com.turtywurty.railroad.util.Utils;
 import com.turtywurty.railroad.window.ConfirmWindow;
-import com.turtywurty.railroad.window.RailroadMenuItem;
+import com.turtywurty.railroad.window.RailroadMenu;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-//TODO: Split some of this off into seperate classes to avoid this main class being a giant monstrosity!
+//TODO: Split some of this off into seperate classes to avoid this main class being a giant monstrosity! - I (NinjaEevee) have moved the Menu into a seperate class
 public class RailroadIDE extends Application {
 
 	private Scene mainScene;
@@ -52,7 +50,7 @@ public class RailroadIDE extends Application {
 		MenuBar menuBar = new MenuBar();
 
 		Menu fileMenu = new Menu("File");
-		fileMenu.getItems().add(this.createNewMenu());
+		fileMenu.getItems().add(RailroadMenu.createNewMenu());
 
 		Menu editMenu = new Menu("Edit");
 		Menu sourceMenu = new Menu("Source");
@@ -73,48 +71,48 @@ public class RailroadIDE extends Application {
 
 	// TODO: Make proper textures for all these icons. Currently all just programmer art! ;)
 	// TODO: Find a way that allows us to fit an image larger than 20x20 pixels into the space this image is drawn.
-	public Menu createNewMenu() {
-		Menu menu = new Menu("New");
-
-		MenuItem javaProject = RailroadMenuItem.Builder.create("Java Project")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/java_project.png")).build();
-
-		MenuItem project = RailroadMenuItem.Builder.create("Project")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/project.png")).build();
-
-		// TODO: Once added texture at "/assets/img/working_set.png" un-comment
-		// setGraphic
-		MenuItem javaWorkingSet = RailroadMenuItem.Builder.create("Java Working Set")
-				/* .setGraphic(Utils.createMenuGraphics("/assets/img/working_set.png") */.build();
-
-		MenuItem packagE = RailroadMenuItem.Builder.create("Package")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/package.png")).build();
-
-		MenuItem sourceFolder = RailroadMenuItem.Builder.create("Source Folder")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/source_folder.png")).build();
-
-		MenuItem file = RailroadMenuItem.Builder.create("File").setGraphic(Utils.createMenuGraphics("/assets/img/file.png"))
-				.build();
-
-		MenuItem folder = RailroadMenuItem.Builder.create("Folder")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/folder.png")).build();
-
-		MenuItem clazz = RailroadMenuItem.Builder.create("Class")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/class.png")).build();
-
-		MenuItem interfacE = RailroadMenuItem.Builder.create("Interface")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/interface.png")).build();
-
-		MenuItem enuM = RailroadMenuItem.Builder.create("Enum").setGraphic(Utils.createMenuGraphics("/assets/img/enum.png"))
-				.build();
-
-		MenuItem annotation = RailroadMenuItem.Builder.create("Annotation")
-				.setGraphic(Utils.createMenuGraphics("/assets/img/annotation.png")).build();
-
-		menu.getItems().addAll(javaProject, project, javaWorkingSet, new SeparatorMenuItem(), sourceFolder, packagE, file,
-				folder, new SeparatorMenuItem(), clazz, interfacE, enuM, annotation);
-		return menu;
-	}
+//	public Menu createNewMenu() {
+//		Menu menu = new Menu("New");
+//
+//		MenuItem javaProject = RailroadMenuItem.Builder.create("Java Project")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/java_project.png")).build();
+//
+//		MenuItem project = RailroadMenuItem.Builder.create("Project")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/project.png")).build();
+//
+//		// TODO: Once added texture at "/assets/img/working_set.png" un-comment
+//		// setGraphic
+//		MenuItem javaWorkingSet = RailroadMenuItem.Builder.create("Java Working Set")
+//				/* .setGraphic(Utils.createMenuGraphics("/assets/img/working_set.png") */.build();
+//
+//		MenuItem packagE = RailroadMenuItem.Builder.create("Package")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/package.png")).build();
+//
+//		MenuItem sourceFolder = RailroadMenuItem.Builder.create("Source Folder")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/source_folder.png")).build();
+//
+//		MenuItem file = RailroadMenuItem.Builder.create("File").setGraphic(Utils.createMenuGraphics("/assets/img/file.png"))
+//				.build();
+//
+//		MenuItem folder = RailroadMenuItem.Builder.create("Folder")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/folder.png")).build();
+//
+//		MenuItem clazz = RailroadMenuItem.Builder.create("Class")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/class.png")).build();
+//
+//		MenuItem interfacE = RailroadMenuItem.Builder.create("Interface")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/interface.png")).build();
+//
+//		MenuItem enuM = RailroadMenuItem.Builder.create("Enum").setGraphic(Utils.createMenuGraphics("/assets/img/enum.png"))
+//				.build();
+//
+//		MenuItem annotation = RailroadMenuItem.Builder.create("Annotation")
+//				.setGraphic(Utils.createMenuGraphics("/assets/img/annotation.png")).build();
+//
+//		menu.getItems().addAll(javaProject, project, javaWorkingSet, new SeparatorMenuItem(), sourceFolder, packagE, file,
+//				folder, new SeparatorMenuItem(), clazz, interfacE, enuM, annotation);
+//		return menu;
+//	}
 
 	private void onClose(Stage window) {
 		boolean shouldClose = ConfirmWindow.displayWindow("Quit", "Are you sure you would like to quit?");
---
+++ src/main/java/com/turtywurty/railroad/window/RailroadMenu.java
@@ -0,0 +1,55 @@
+package com.turtywurty.railroad.window;
+
+import com.turtywurty.railroad.util.Utils;
+
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+
+//TODO: Possibly re-name this class, I just used RailroadMenu because i couldn't use Menu (the imports were messed up)
+public class RailroadMenu {
+//TODO: Make proper textures for all these icons. Currently all just programmer art! ;)
+	// TODO: Find a way that allows us to fit an image larger than 20x20 pixels into the space this image is drawn.
+	public static Menu createNewMenu() {
+		Menu menu = new Menu("New");
+
+		MenuItem javaProject = RailroadMenuItem.Builder.create("Java Project")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/java_project.png")).build();
+
+		MenuItem project = RailroadMenuItem.Builder.create("Project")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/project.png")).build();
+
+		// TODO: Once added texture at "/assets/img/working_set.png" un-comment
+		// setGraphic
+		MenuItem javaWorkingSet = RailroadMenuItem.Builder.create("Java Working Set")
+				/* .setGraphic(Utils.createMenuGraphics("/assets/img/working_set.png") */.build();
+
+		MenuItem packagE = RailroadMenuItem.Builder.create("Package")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/package.png")).build();
+
+		MenuItem sourceFolder = RailroadMenuItem.Builder.create("Source Folder")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/source_folder.png")).build();
+
+		MenuItem file = RailroadMenuItem.Builder.create("File").setGraphic(Utils.createMenuGraphics("/assets/img/file.png"))
+				.build();
+
+		MenuItem folder = RailroadMenuItem.Builder.create("Folder")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/folder.png")).build();
+
+		MenuItem clazz = RailroadMenuItem.Builder.create("Class")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/class.png")).build();
+
+		MenuItem interfacE = RailroadMenuItem.Builder.create("Interface")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/interface.png")).build();
+
+		MenuItem enuM = RailroadMenuItem.Builder.create("Enum").setGraphic(Utils.createMenuGraphics("/assets/img/enum.png"))
+				.build();
+
+		MenuItem annotation = RailroadMenuItem.Builder.create("Annotation")
+				.setGraphic(Utils.createMenuGraphics("/assets/img/annotation.png")).build();
+
+		menu.getItems().addAll(javaProject, project, javaWorkingSet, new SeparatorMenuItem(), sourceFolder, packagE, file,
+				folder, new SeparatorMenuItem(), clazz, interfacE, enuM, annotation);
+		return menu;
+	}
+}
