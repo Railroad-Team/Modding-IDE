ecdc6bfdc6d0d16ef1603afa7d2283c918aa8f36
Cy4Shot <veschi@gmail.com>
2021-02-08 09:39:38 +0000

    Config / Language Support

---
+++ src/main/java/com/turtywurty/railroad/config/Config.java
@@ -0,0 +1,10 @@
+package com.turtywurty.railroad.config;
+
+public class Config {
+	public LanguageConfig lang;
+	
+	public Config() {
+		// TODO select custom language file, using en_us.json for now.
+		this.lang = new LanguageConfig("/assets/lang/en_us.json"); // Yes, this is the same way minecraft does it. Why not?
+	}
+}
