1fe1a76e074f78112a6d5e71f00123123c6ac3f8
Cy4Shot <veschi@gmail.com>
2021-02-09 10:34:25 +0000

    Syntax Config

--- src/main/java/com/turtywurty/railroad/config/Configs.java
+++ src/main/java/com/turtywurty/railroad/config/Configs.java
@@ -3,16 +3,10 @@ package com.turtywurty.railroad.config;
 public class Configs {
 
 	public LanguageConfig lang;
+	public SyntaxConfig syntax;
 
 	public Configs() {
 		this.lang = new LanguageConfig("/assets/lang/en_us.json");
-	}
-	
-	public LanguageConfig getLang() {
-		return this.lang;
-	}
-	
-	public void setLang(LanguageConfig lang) {
-		this.lang = lang;
+		this.syntax = new SyntaxConfig();
 	}
 }
