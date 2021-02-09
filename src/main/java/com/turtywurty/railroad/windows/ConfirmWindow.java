590b9c29d5cbc111316b31bfb46e6052d8cc824d
Cy4Shot <veschi@gmail.com>
2021-02-09 08:22:14 +0000

    Update ConfirmWindow.java

--- src/main/java/com/turtywurty/railroad/windows/ConfirmWindow.java
+++ src/main/java/com/turtywurty/railroad/windows/ConfirmWindow.java
@@ -16,6 +16,7 @@ import javafx.stage.Stage;
 // TODO: Come up with a cleaner way of doing this. This code was rushed.
 public class ConfirmWindow {
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static Map<Stage, Boolean> ANSWER = new HashMap();
 
 	public static boolean displayWindow(String title, String message) {
