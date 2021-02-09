1725b7b7b0990906a29cc123f5bd4eb56b4d0386
Saksham Kumar <saksham.vvip@gmail.com>
2021-02-09 20:13:48 +0530

    improved new file support (#15)
    * Added New File Support
    
    The gui is definitley WIP there is a fileUtils which can is self-explanatory
    
    * Updated
    
    added FileDialog
    
    * Delete src/main/java/com/turtywurty/railroad/window directory
    
    * f
    
    * New file
    
    added createNewJavaFile
    which auto generates code depending if u click create interface, class or enum

--- src/main/java/com/turtywurty/railroad/windows/CreateNewFileWindow.java
+++ src/main/java/com/turtywurty/railroad/windows/CreateNewFileWindow.java
@@ -1,72 +1,26 @@
 package com.turtywurty.railroad.windows;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
-import com.turtywurty.railroad.util.FileUtils;
-import com.turtywurty.railroad.util.UIUtils;
-
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.Modality;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-//TODO: make chosing file path have a filedialog screen 
-public class CreateNewFileWindow {
-	public static Map<Stage, Boolean> ANSWER = new HashMap<Stage, Boolean>();
-	public static String filePath;
-
-	public static void displayWindow(String title, String message) {
-		Stage window = new Stage();
-		window.centerOnScreen();
-		window.initModality(Modality.APPLICATION_MODAL);
-		window.setTitle(title);
-		window.setMinWidth(250);
-		window.setMinHeight(100);
-		window.setResizable(false);
-		DirectoryChooser directoryChooser = new DirectoryChooser();
-
-		Label label = new Label(message);
-
-		TextField fileName = new TextField("");
-		TextField pathName = new TextField("File Path");
-		pathName.setEditable(false);
-		Button choseFilePath = UIUtils.createButton("Chose Path", event -> {
-
-			File file = directoryChooser.showDialog(window);
-			if (file != null) {
-				filePath = file.getAbsolutePath();
-				pathName.setText(filePath);
-			}
-			directoryChooser.setInitialDirectory(new File(""));
-
-		});
-
-		Button yesBtn = UIUtils.createButton("Create File", event -> {
-
-			System.out.println(filePath);
-			FileUtils.createNewFile(filePath + "\\" + fileName.getText());
-			window.close();
-		});
-
-		Button noBtn = UIUtils.createButton("Cancel", event -> {
-			ANSWER.put(window, false);
-			window.close();
-		});
-
-		VBox layout = new VBox(10);
-		layout.getChildren().addAll(label, fileName, choseFilePath, pathName, yesBtn, noBtn);
-		layout.setAlignment(Pos.CENTER);
-
-		Scene scene = new Scene(layout);
-		window.setScene(scene);
-		window.showAndWait();
-
+ 
+public class CreateNewFileWindow extends AbstractNewFileWindow{
+	public CreateNewFileWindow(String title, String message) {
+		super(title, message);
 	}
+
+	@Override
+	public void fileDialogBox(Stage window) {
+		FileChooser fileChooser = new FileChooser();
+		
+		File file = fileChooser.showSaveDialog(window);
+		if (file != null) {
+			filePath = file.getAbsolutePath();
+			this.pathName.setText(filePath);
+		}
+		fileChooser.setInitialDirectory(new File(""));
+	}
+	
 }
