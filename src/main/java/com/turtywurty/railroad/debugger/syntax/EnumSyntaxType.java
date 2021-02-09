babd723c2803fbca89cf5433fcabec82b5081fbf
Cy4Shot <veschi@gmail.com>
2021-02-09 14:45:57 +0000

    a

--- src/main/java/com/turtywurty/railroad/debugger/syntax/EnumSyntaxType.java
+++ src/main/java/com/turtywurty/railroad/debugger/syntax/EnumSyntaxType.java
@@ -2,5 +2,5 @@ package com.turtywurty.railroad.debugger.syntax;
 
 // Add more. This is very bare
 public enum EnumSyntaxType {
-	TEXT, KEYWORD, NAME, LITERAL, STRING, NUMBER, OPERATORS, PUNC, COMMENT, ELSE, FUNCTION
+	KEYWORD, LITERAL, STRING, NUMBER, COMMENT, ELSE, FUNCTION, CLASS
 }
