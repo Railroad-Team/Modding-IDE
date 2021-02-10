ed69609f47859e309049634aa1e5d49be1c3a546
Temedy491 <dave.suchanek@email.cz>
2021-02-10 17:44:53 +0100

    ~ Deletion of useless things

--- src/main/java/io/github/railroad/debugger/syntax/EnumSyntaxType.java
+++
@@ -1,8 +0,0 @@
-package io.github.railroad.debugger.syntax;
-
-//TODO: get rid of this
-
-// Add more. This is very bare
-public enum EnumSyntaxType {
-    KEYWORD, LITERAL, STRING, NUMBER, COMMENT, ELSE, FUNCTION, CLASS
-}
--- src/main/java/io/github/railroad/editor/Entity.java
+++ src/main/java/io/github/railroad/editor/Entity.java
@@ -3,6 +3,7 @@ package io.github.railroad.editor;
 import javafx.scene.shape.Box;
 import javafx.scene.transform.Rotate;
 
+//TODO: get rid of this
 public final class Entity {
     public final Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
     public final Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
