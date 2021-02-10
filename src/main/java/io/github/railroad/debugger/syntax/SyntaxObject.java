51d1966f5dcb04cc6cfed6e287183ed65d518e6d
Cy4Shot <veschi@gmail.com>
2021-02-10 18:35:52 +0000

    New Syntax System - Completely Scriptable

--- src/main/java/io/github/railroad/debugger/syntax/SyntaxObject.java
+++ src/main/java/io/github/railroad/debugger/syntax/SyntaxObject.java
@@ -6,24 +6,27 @@ import java.util.regex.Pattern;
 public class SyntaxObject {
 
 	private String ext;
-	private Map<String, EnumSyntaxType> regex;
+	private String path;
+	private Map<String, String> regex;
 	private Pattern compiled;
 
-	public SyntaxObject(String e, Map<String, EnumSyntaxType> r) {
+	public SyntaxObject(String path, String e, Map<String, String> r) {
 		this.ext = e;
 		this.regex = r;
+		this.setPath(path);
 		compile();
 	}
 
 	public void compile() {
 		StringBuilder builder = new StringBuilder();
-		for (Map.Entry<String, EnumSyntaxType> e : this.regex.entrySet()) {
-			builder.append("|(?<").append(e.getValue().name()).append(">").append(e.getKey()).append(")");
+		for (Map.Entry<String, String> e : this.regex.entrySet()) {
+			builder.append("|(?<").append(e.getValue()).append(">").append(e.getKey()).append(")");
 		}
+		System.out.println(builder.toString());
 		this.compiled = Pattern.compile(builder.substring(1)); // Remove first "|"
 	}
 
-	public boolean hasRegexFor(EnumSyntaxType type) {
+	public boolean hasRegexFor(String type) {
 		return regex.containsValue(type);
 	}
 
@@ -35,11 +38,11 @@ public class SyntaxObject {
 		this.ext = ext;
 	}
 
-	public Map<String, EnumSyntaxType> getRegex() {
+	public Map<String, String> getRegex() {
 		return regex;
 	}
 
-	public void setRegex(Map<String, EnumSyntaxType> regex) {
+	public void setRegex(Map<String, String> regex) {
 		this.regex = regex;
 	}
 
@@ -49,6 +52,14 @@ public class SyntaxObject {
 
 	public void setCompiled(Pattern compiled) {
 		this.compiled = compiled;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 }
