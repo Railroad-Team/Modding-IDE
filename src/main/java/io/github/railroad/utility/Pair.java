dc64710370f20c5db7773ddc78cb272caf9729af
Temedy491 <dave.suchanek@email.cz>
2021-02-11 13:56:21 +0100

    ~ Templates.java fix

--- src/main/java/io/github/railroad/utility/Pair.java
+++
@@ -1,7 +0,0 @@
-package io.github.railroad.utility;
-
-public class Pair<First, Second> {
-    public final First first;
-    public final Second second;
-    public Pair(First first, Second second) {this.first = first; this.second = second;}
-}
