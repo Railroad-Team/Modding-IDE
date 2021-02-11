2b6ed064042e3fe75da4d66839a0093c2fc432b5
Cy4Shot <veschi@gmail.com>
2021-02-11 10:37:44 +0000

    Update Startup.java

--- src/main/java/io/github/railroad/Startup.java
+++ src/main/java/io/github/railroad/Startup.java
@@ -1,13 +1,10 @@
 package io.github.railroad;
 
-import io.github.railroad.config.Configs;
-import io.github.railroad.debugger.syntax.SyntaxHandler;
-
 public class Startup {
 
 	public static void main(String[] args) {
-//		Railroad.boot(args);
-		new Configs();
-		SyntaxHandler.boot(args);
+		Railroad.boot(args);
+//		new Configs();
+//		SyntaxHandler.boot(args);
 	}
 }
