60869733a85e6d52b9117e3fd87cc935e4af637c
TheOnlyTails <shacharzidon@gmail.com>
2021-02-15 16:17:06 +0200

    Replaced the old localization system with resource bundles. Moved all the old json lang file to .properties files.

--- src/main/java/io/github/railroad/lang/LangManger.java
+++ src/main/java/io/github/railroad/lang/LangManger.java
@@ -1,14 +1,27 @@
 package io.github.railroad.lang;
 
-import io.github.railroad.Railroad;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
+/**
+ * @author TheOnlyTails
+ */
 public interface LangManger {
-    @SuppressWarnings("ConstantConditions")
-    JSONObject ENGLISH = new JSONObject(new JSONTokener(Railroad.class.getResourceAsStream("/assets/lang/en_us.json")));
+    ResourceBundle ENGLISH = ResourceBundle.getBundle("assets.lang.en", Locale.ENGLISH);
+    ResourceBundle ITALIAN = ResourceBundle.getBundle("assets.lang.it", new Locale("it"));
+    ResourceBundle SPANISH = ResourceBundle.getBundle("assets.lang.es", new Locale("es"));
+    ResourceBundle TAIWAN_CHINESE = ResourceBundle.getBundle("assets.lang.zh", new Locale("zh"));
 
-    static String getLocalization(String localizationString, JSONObject localization) {
-        return localization.getString(localizationString);
+    static String getLocalization(String key, ResourceBundle bundle) {
+        try {
+            /*
+             TODO: once we have a working settings menu, remove the bundle parameter and use the bundle set in the settings
+             */
+            return bundle.getString(key);
+        } catch (MissingResourceException e) {
+            System.err.printf("Localization key %s was not found.\n", key);
+            return key;
+        }
     }
 }
